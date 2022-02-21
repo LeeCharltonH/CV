870431eda77381f12dea455f3704064a6fcf1f6d
Lee Charlton-Hassall <leehassall@hotmail.co.uk>
2022-02-21 21:17:28 +0000

    Added GA tracking.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-221130153-1');
 
 
 ReactDOM.render(
